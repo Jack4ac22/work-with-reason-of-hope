47c0d7a027acaf0cfdda58defa56c07f83f991ea
Jack Kazanjyan <Jack4ac22@icloud.com>
2024-05-02 00:03:00 +0200

    "Integrating assets and implementing Dark Mode switch functionality"

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import DarkModeSwitch from "@/components/design/ui/dark-mode-switch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <DarkModeSwitch>{children}</DarkModeSwitch>
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,42 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+      colors: {
+        lightShade: "#F8F6F6",
+        lightAccent: "#AFB7C5",
+        mainBrand: "#645D64",
+        darkAccent: "#8993BB",
+        darkShade: "#222541",
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
+      },
+      transitionDuration: {
+        2000: "2000ms",
+      },
+      animation: {
+        "spin-slow": "spin 3s linar infinite",
+        wigle: "wigle 1s ease-in-out infinite",
+      },
+      keyframes: {
+        wigle: {
+          "0%,100%": { transform: "rotate(0deg)", opacity: 1, scale: 1 },
+          "50%": { transform: "rotate(180deg)", opacity: 0.8, scale: 1.1 },
+        },
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
       },
     },
   },
   plugins: [],
+  darkMode: "class",
 };
