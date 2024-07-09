00fd9b90ca78f1b48de75b8c4ee06ea1b78b763d
Jack Kazanjyan <jack4ac22@yahoo.com>
2024-07-09 23:18:31 +0200

    staging card design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,25 @@
 import { getAllBlogArticles } from "@/utils/blog/updated-functions";
-import { ArticleCard } from "@/components/blog-components/general-compenents/cards-list/article-card";
 import Image from 'next/image';
 export default function Home() {
   const articles = getAllBlogArticles();
   return (
     <>
       <h1>Home Page</h1>
-      {articles.map((article) => (
-        <>
-          <div key={article.title}>
-            <h1>{article.title}</h1>
-            {/* <DynamicImageComponent imageName={article.coverImage} /> */}
-            <Image src={`/blog_images/${article.coverImage}`} alt={`Cover Image for: ${article.slug}`} width={860} height={360} loading="lazy" />
-          </div></>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 p-2 m-2 gap-2 md:gap-3 lg:gap-4">
+        {articles.map((article) => (
+          <>
+            <div key={article.title} className="flex flex-col items-center cont max-w-sm p-2 border-2 rounded-xl hover:shadow-lg h-128 hover:overflow-y-auto">
+              <div className="roundeds-xl">
+                <Image src={`/blog_images/${article.coverImage}`} alt={`Cover Image for: ${article.slug}`} width={860} height={360} loading="lazy" 
+                className="rounded-t-xl object-contain"/>
+              </div>
 
-      ))}
+              <h1 className="font-bold center">{article.title}</h1>
+              <p className="text-md">{article.description}</p>
+            </div></>
+
+        ))}
+      </div>
     </>
   );
 }
--- src/components/images-components/Image-import.jsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-export default function DynamicImageComponent({ imageName }) {
-  let imagePath = `/src/assets/blog-images/${imageName}`;
-
-  return (
-    <Image src={imagePath} alt="Dynamic Image" />
-  );
-}
