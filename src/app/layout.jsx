5ccd5dd9ee0a029bc7ffc33acfbb2fa4123e0e8c
Jack Kazanjyan <jack4ac22@yahoo.com>
2024-07-22 22:07:40 +0200

    add markazi font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,25 @@
-import { Inter } from "next/font/google";
+import { Markazi_Text, Roboto } from "next/font/google";
 import "@/assets/styles/globals.css";
 import NavBar from "@/components/blog-components/ui/navigation/navbar";
 import LayOverSection from "@/components/blog-components/ui/layover/layover";
 import { LayoverGlobalProvider } from "@/context/layover/LayoverGlobalContext";
 // import DarkModeSwitch from "@/components/blog-components/ui/dark-mode/dark-mode-switch";
 
-const inter = Inter({ subsets: ["latin"] });
+const markazi_text = Markazi_Text(
+  {
+    weight: ['400', '500', '600', '700'],
+    subsets: ['arabic'],
+    display: 'swap',
+  }
+);
+
+const roboto = Roboto(
+  {
+    weight: ['100', '300', '400', '500', '700'],
+    subsets: ['latin'],
+    display: 'swap',
+  }
+);
 
 export const metadata = {
   title: "Reson Of Hope |  سبب الرجاء",
@@ -14,7 +28,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ar" dir="rtl" className="scroll-smooth">
+    <html lang="ar" dir="rtl" className={`"scroll-smooth" ${markazi_text.className}`}>
       <body className="transition-all duration-200">
         <LayoverGlobalProvider>
           {/* <DarkModeSwitch > */}
